f73779255eac97e3d4a750078b3e29ace8660c44
CrSandr8 <sandr8@DESKTOP-UT9CVUQ.localdomain>
2026-04-05 19:15:10 +0200

    nvm

--- src/virtual_fat/fat.c
+++ src/virtual_fat/fat.c
@@ -2,26 +2,31 @@
 
 extern FAT_Disk disk;
 
-int find_DirectoryEntry(const char *path, FAT_DirectoryEntry *output){
-
+int find_DirectoryEntry(const char *path, FAT_FCB *output)
+{
+    
 }
 
-int chain_append(){}
+int chain_append() {}
 
-int chain_rm(uint32_t first_cluster){
+int chain_rm(uint32_t first_cluster)
+{
     uint32_t current = first_cluster;
 
-    if (current == FAT_FREE) return FAT_SUCCESS;
+    if (current == FAT_FREE)
+        return FAT_SUCCESS;
 
-    while (1) {
-        uint32_t next = disk.fat_table[current]; // 
-        disk.fat_table[current] = FAT_FREE;      // 
-        
-        if (next == FAT_EOC || next == FAT_FREE) {
+    while (1)
+    {
+        uint32_t next = disk.fat_table[current]; //
+        disk.fat_table[current] = FAT_FREE;      //
+
+        if (next == FAT_EOC || next == FAT_FREE)
+        {
             break; //
         }
-        
-        current = next; // 
+
+        current = next; //
     }
 
     return FAT_SUCCESS;
@@ -29,14 +34,14 @@ int chain_rm(uint32_t first_cluster){
 
 int fat_format(const char *filename, int size)
 {
-
 }
 
 int fat_mount(const char *filename)
 {
     printf("Starting mount operations...\n");
     int fd;
-    if ((fd = open(filename, O_RDWR)) == -1){
+    if ((fd = open(filename, O_RDWR)) == -1)
+    {
         perror("Error in opening file during mount...");
         return FAT_ERR_GENERIC;
     }
@@ -45,16 +50,18 @@ int fat_mount(const char *filename)
 
     disk.disk_base = mmap(NULL, disk.disk_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-    if (disk.disk_base == MAP_FAILED){
+    if (disk.disk_base == MAP_FAILED)
+    {
         perror("Error while mapping...");
         return FAT_ERR_GENERIC;
     }
 
-    disk.bs = (FAT_BootSector *) disk.disk_base;
+    disk.bs = (FAT_BootSector *)disk.disk_base;
 
     printf("Checking disk signature...\n");
 
-    if (disk.bs->Signature != BS_SIGNATURE){
+    if (disk.bs->Signature != BS_SIGNATURE)
+    {
         munmap(disk.disk_base, disk.disk_size);
         disk.disk_base = NULL;
         close(fd);
@@ -68,15 +75,16 @@ int fat_mount(const char *filename)
     // NOTE: the following offsets are in bytes
 
     uint32_t fsi_offset = disk.bs->BytsPerSec * disk.bs->FSInfo_offset;
-    disk.fsinfo = (FAT_FSInfo *) (disk.disk_base + fsi_offset);
+    disk.fsinfo = (FAT_FSInfo *)(disk.disk_base + fsi_offset);
 
     uint32_t fat_offset = disk.bs->BytsPerSec * disk.bs->RsvdSecCnt;
-    disk.fat_table = (uint32_t *) (disk.disk_base + fat_offset);
+    disk.fat_table = (uint32_t *)(disk.disk_base + fat_offset);
 
     uint32_t data_offset = (disk.bs->RsvdSecCnt + (disk.bs->NumFATS * disk.bs->FATSz)) * disk.bs->BytsPerSec;
-    disk.data_region = (uint8_t *) (disk.disk_base + data_offset);
+    disk.data_region = (uint8_t *)(disk.disk_base + data_offset);
 
-    for (int i = 0; i < MAX_OPEN_FILES; i++) {
+    for (int i = 0; i < MAX_OPEN_FILES; i++)
+    {
         disk.open_files[i].is_used = 0;
         disk.open_files[i].current_offset = 0;
     }
@@ -91,14 +99,17 @@ void fat_unmount(FAT_Disk disk) // TODO capire se qui ci deve essere un input o 
 {
 
     printf("Starting unmount process...\n");
-    if (disk.disk_base == NULL) return;
+    if (disk.disk_base == NULL)
+        return;
 
-    if (msync(disk.disk_base, disk.disk_size, MS_SYNC) == -1){
+    if (msync(disk.disk_base, disk.disk_size, MS_SYNC) == -1)
+    {
         perror("Error while saving data on disk");
         return FAT_ERR_GENERIC;
     }
 
-    if (munmap(disk.disk_base, disk.disk_size) == -1){
+    if (munmap(disk.disk_base, disk.disk_size) == -1)
+    {
         perror("Error while unmapping disk");
         return FAT_ERR_GENERIC;
     }
@@ -107,33 +118,35 @@ void fat_unmount(FAT_Disk disk) // TODO capire se qui ci deve essere un input o 
     printf("Unmounting OK!\n");
 
     return FAT_SUCCESS;
-
 }
 int fat_init(const char *filename)
 {
-    if (fat_mount(filename) == FAT_SUCCESS) return FAT_SUCCESS;
+    if (fat_mount(filename) == FAT_SUCCESS)
+        return FAT_SUCCESS;
 
-    //might want to format or do some
-
+    // might want to format or do some
 }
 int fat_open(const char *path, int mode)
 {
-    //printf("[DEBUG] MOCK: fat_open chiamato con path '%s' e mode %d\n", path, mode);
+    // printf("[DEBUG] MOCK: fat_open chiamato con path '%s' e mode %d\n", path, mode);
 
-    //return 3;
+    // return 3;
 
-    FAT_DirectoryEntry found;
+    FAT_FCB found;
 
-    if (find_DirectoryEntry(path, &found) == FAT_ERR_GENERIC){
+    if (find_DirectoryEntry(path, &found) == FAT_ERR_GENERIC)
+    {
 
-        //TODO aggiungere controllo sulla modalità di apertura del file
+        // TODO aggiungere controllo sulla modalità di apertura del file
 
         perror("Cannot open directory: file not found");
         return FAT_ERR_GENERIC;
     }
 
-    for(int i = 0; i < MAX_OPEN_FILES; i++){
-        if (disk.open_files[i].is_used == 0){
+    for (int i = 0; i < MAX_OPEN_FILES; i++)
+    {
+        if (disk.open_files[i].is_used == 0)
+        {
             disk.open_files[i].is_used = 1;
             disk.open_files[i].cached_entry = found;
             disk.open_files[i].current_offset = 0;
@@ -146,40 +159,41 @@ int fat_open(const char *path, int mode)
 }
 int fat_close(int fd)
 {
-    //printf("[DEBUG] MOCK: fat_close chiamato per chiudere l'fd %d\n", fd);
-    //return FAT_SUCCESS;
+    // printf("[DEBUG] MOCK: fat_close chiamato per chiudere l'fd %d\n", fd);
+    // return FAT_SUCCESS;
 
-    if (fd > MAX_OPEN_FILES || fd <= 0){
+    if (fd > MAX_OPEN_FILES || fd <= 0)
+    {
         perror("Error: file not open");
         return FAT_ERR_GENERIC;
     }
 
-    disk.open_files[fd].is_used = 0;    
+    disk.open_files[fd].is_used = 0;
 
     disk.open_files[fd].current_offset = 0;
 
-    memset(&disk.open_files[fd].cached_entry, 0, sizeof(FAT_DirectoryEntry)); // Clearing cached entry
-    
+    memset(&disk.open_files[fd].cached_entry, 0, sizeof(FAT_FCB)); // Clearing cached entry
+
     return FAT_SUCCESS;
 }
 int fat_read(int fd, void *buf, int size)
 {
     printf("[DEBUG] MOCK: fat_read chiamato per l'fd %d, richiedendo %d byte\n", fd, size);
-    
+
     // Inseriamo una finta stringa di testo nel buffer giusto per testare il comando 'cat'
-    if (buf != NULL && size > 0) {
-        strncpy((char*)buf, "Contenuto fittizio del file lettto con successo!\n", size);
+    if (buf != NULL && size > 0)
+    {
+        strncpy((char *)buf, "Contenuto fittizio del file lettto con successo!\n", size);
     }
-    
+
     // Fingiamo di aver letto esattamente il numero di byte richiesti (o almeno un po')
     return size;
 }
 int fat_write(int fd, const void *buf, int size)
 {
-    //printf("[DEBUG] MOCK: fat_write chiamato per l'fd %d. Richiesta scrittura di %d byte\n", fd, size);
-    // Fingiamo di aver scritto tutti i byte con successo
-    //return size;
-    
+    // printf("[DEBUG] MOCK: fat_write chiamato per l'fd %d. Richiesta scrittura di %d byte\n", fd, size);
+    //  Fingiamo di aver scritto tutti i byte con successo
+    // return size;
 }
 int fat_lseek(int fd, int offset, int whence)
 {
@@ -194,19 +208,19 @@ int fat_mkdir(const char *path)
 }
 int fat_rm(const char *path)
 {
-    //printf("[DEBUG] MOCK: fat_rm chiamato per eliminare il file/dir '%s'\n", path);
-    //return FAT_SUCCESS;
-    FAT_DirectoryEntry out;
+    // printf("[DEBUG] MOCK: fat_rm chiamato per eliminare il file/dir '%s'\n", path);
+    // return FAT_SUCCESS;
+    FAT_FCB out;
 
-    if (find_DirectoryEntry(path, &out) == FAT_ERR_GENERIC){
+    if (find_DirectoryEntry(path, &out) == FAT_ERR_GENERIC)
+    {
         perror("Error: file or directory with input path not found");
         return FAT_ERR_GENERIC;
     }
 
-    if (chain_rm(out.first_cluster) == FAT_ERR_GENERIC){
+    if (chain_rm(out.first_cluster) == FAT_ERR_GENERIC)
+    {
         perror("Error while trying to remove related clusters");
         return FAT_ERR_GENERIC;
     }
-
-
 }
--- src/virtual_fat/fat_structs.h
+++ src/virtual_fat/fat_structs.h
@@ -4,9 +4,9 @@
 #include <stdint.h>
 
 // Error codes for function return values
-#define FAT_SUCCESS           0
-#define FAT_ERR_GENERIC      -1   // Generic error code 
-#define FAT_ERR_BAD_SIG      -2   // Non mountable file
+#define FAT_SUCCESS 0
+#define FAT_ERR_GENERIC -1 // Generic error code
+#define FAT_ERR_BAD_SIG -2 // Non mountable file
 
 // Dimensional values used
 #define SECTOR_SIZE 512
@@ -14,17 +14,10 @@
 // Personalized signature
 #define BS_SIGNATURE 0xAC18
 
-// File attributes, as bitmask
-#define ATTR_READ_ONLY  0x01
-#define ATTR_HIDDEN     0x02
-#define ATTR_SYSTEM     0x04
-#define ATTR_VOLUME_ID  0x08
-#define ATTR_DIRECTORY  0x10    // Is this a directory?
-
 // Block status
 #define FAT_FREE 0
 #define FAT_RESERVED 0x00000001
-#define FAT_INUSE(x) ((x>=0x00000002) && (x<=0x0FFFFFF6)) 
+#define FAT_INUSE(x) ((x >= 0x00000002) && (x <= 0x0FFFFFF6))
 #define FAT_BADCLUSTER 0x0FFFFFF7
 #define FAT_EOC 0x0FFFFFF8
 
@@ -32,72 +25,65 @@
 #define MAX_OPEN_FILES 12
 
 //
-#define  DISK_SIZE 66666666
-
+#define DISK_SIZE 66666666
 
 // BOOT SECTOR
 
 typedef struct
 {
 
-    uint16_t BytsPerSec;     // Sector size in unit of byte  This value may take on only the following values: 512, 1024, 2048 or 4096
-    uint8_t  SecPerClus;     // Sector per allocation unit This value must be a power of 2 that is greater than 0. The legal values are 1, 2, 4, 8, 16, 32, 64, and 128
-    uint16_t RsvdSecCnt;     //TODO explain 
-    uint8_t  NumFATS;        // How many FATS we have
-    uint32_t FATSz;        // Size of a FAT in unit of sector.
-    uint32_t RootClus;       // First cluster number of root directory (Usually 2)
-    uint16_t FSInfo_offset;  // Sector of FSInfo structure in offset from top of the FAT volume (Usually 1)
+    uint16_t BytsPerSec;    // Sector size in unit of byte  This value may take on only the following values: 512, 1024, 2048 or 4096
+    uint8_t SecPerClus;     // Sector per allocation unit This value must be a power of 2 that is greater than 0. The legal values are 1, 2, 4, 8, 16, 32, 64, and 128
+    uint16_t RsvdSecCnt;    // TODO explain
+    uint8_t NumFATS;        // How many FATS we have
+    uint32_t FATSz;         // Size of a FAT in unit of sector.
+    uint32_t RootClus;      // First cluster number of root directory (Usually 2)
+    uint16_t FSInfo_offset; // Sector of FSInfo structure in offset from top of the FAT volume (Usually 1)
     uint8_t padding[494];
     uint16_t Signature;
 
-} __attribute__((packed)) FAT_BootSector; // 512 bytes 
+} __attribute__((packed)) FAT_BootSector; // 512 bytes
 
 typedef struct
 {
-    uint32_t FSI_LeadSig;        // 0x41615252 || Whatever
-    uint8_t  FSI_Reserved1[480]; // 0
-    uint32_t FSI_StrucSig;       // 0x61417272 || Whatever
-    uint32_t FSI_Free_Count;     // Last known free cluster count
-    uint32_t FSI_Nxt_Free;       // A hint for the next free cluster
-    uint8_t  FSI_Reserved2[12];  // 0
-    uint32_t FSI_TrailSig;       // 0xAA550000 || Whatever
+    uint32_t FSI_Free_Count;     // Last known free cluster count (4 byte)
+    uint32_t FSI_Nxt_Free;       // A hint for the next free cluster (4 byte)   
+    uint8_t  padding[504];       
     
 } __attribute__((packed)) FAT_FSInfo;  // 512 bytes
 
 typedef struct
 {
-    uint8_t name[22];     // name always has 22 chars
+    uint8_t name[23];       // name always has 22 chars
     uint32_t first_cluster; // first cluster index
-    uint8_t attr;    // directory attributes
-    uint8_t is_res;    // is directory reserved?
+    uint8_t is_dir;         // is this a directory?
     uint32_t file_size;
 
-} __attribute__((packed)) FAT_DirectoryEntry;  // 32 bytes
+} __attribute__((packed)) FAT_FCB; // 32 bytes
 
 typedef struct
 {
     uint8_t is_used;
     uint32_t current_offset;
-    FAT_DirectoryEntry cached_entry;
+    FAT_FCB cached_entry;
     uint8_t padding[27];
 
-} __attribute__((packed)) FAT_Fd;  //64 bytes
+} __attribute__((packed)) FAT_Fd; // 64 bytes
 
 typedef struct
 {
-    uint8_t *disk_base;         
-    uint32_t disk_size;         
-    
-    FAT_BootSector *bs;         
-    FAT_FSInfo *fsinfo;         
-    uint32_t *fat_table;        
+    uint8_t *disk_base;
+    uint32_t disk_size;
+
+    FAT_BootSector *bs;
+    FAT_FSInfo *fsinfo;
+    uint32_t *fat_table;
     uint8_t *data_region;
-    
+
     FAT_Fd open_files[MAX_OPEN_FILES];
 
 } FAT_Disk;
 
 // uint32_t first_data_sector = fs_state.bs->RsvdSecCnt + (fs_state.bs->NumFATS * fs_state.bs->FATSz);
-
 
 #endif
